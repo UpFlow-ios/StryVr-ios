21837aa04cc51b262e1c6e3d73bf1e99cd275dd9
Joe Dormond <upflowapp@gmail.com>
2025-06-16 19:21:43 -0700

    chore: save all local Xcode + scheme changes

--- StryVr/stryvr-app/Package.swift
+++ StryVr/stryvr-app/Package.swift
@@ -1,30 +1,26 @@
 // swift-tools-version:6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "stryvr-app",
     platforms: [
-        .visionOS(.v2),
-        .macOS(.v15),
-        .iOS(.v18)
+        .visionOS(.v1),
+        .macOS(.v13),
+        .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .executable(
             name: "stryvr-app",
-            targets: ["stryvr-app"]),
+            targets: ["stryvr-app"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // Add your future dependencies here
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "stryvr-app",
-            dependencies: []),
+            path: "Sources/stryvr-app"
+        )
     ]
 )
---
+++ StryVr/stryvr-app/Sources/stryvr-app/StryVrApp.swift
@@ -0,0 +1,16 @@
+//
+//  StryVrApp.swift
+//  stryvr-app
+//
+//  Created by Joe Dormond on 6/16/25.
+//
+import SwiftUI
+
+@main
+struct StryVrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView() // or SplashScreenView() if it’s ready
+        }
+    }
+}
