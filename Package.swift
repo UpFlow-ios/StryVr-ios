9896008af6184d5a14ba3311724a3bfd3eebb811
Joe Dormond <upflowapp@gmail.com>
2025-07-30 13:45:33 -0700

    Add missing dependencies: Firebase SDK, Lottie, ConfettiSwiftUI, Kingfisher, XCGLogger for complete build support

--- Package.swift
+++ Package.swift
@@ -13,12 +13,31 @@ let package = Package(
         .executable(
             name: "stryvr-app",
             targets: ["stryvr-app"]
-        ),
+        )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.0.0"),
+        .package(url: "https://github.com/simibac/ConfettiSwiftUI.git", from: "1.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger", from: "7.0.1"),
+    ],
     targets: [
         .target(
             name: "StryVrModule",
+            dependencies: [
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestoreSwift", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "Lottie", package: "lottie-ios"),
+                .product(name: "ConfettiSwiftUI", package: "ConfettiSwiftUI"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
+                .product(name: "XCGLogger", package: "XCGLogger"),
+            ],
             path: "StryVrModule"
         ),
         .executableTarget(
